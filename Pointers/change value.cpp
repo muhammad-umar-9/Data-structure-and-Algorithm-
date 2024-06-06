1dc22339a65294625553a5cf64fd70a6470f7257
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-06-06 20:55:45 +0500

    task

---
+++ Pointers/change value.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      int a[] = {2 , 3 , 4 ,5 , 6 , 7};
+      int n = sizeof(a) / sizeof(a[0]);
+
+      cout<<"orignal array"<<endl;
+
+       for(int i = 0 ; i<n ; i++)
+      {
+            
+            cout<<a[i]<<"  ";
+      }
+      cout<<endl;
+      for(int i = 0 ; i<n ; i++)
+      {
+            
+            if(i == 2)
+            {
+                  a[i] = 15;
+            }
+      }
+      cout<<"Modified array"<<endl;
+      
+      for(int i = 0 ; i<n ; i++)
+      {
+            
+            cout<<a[i]<<"  ";
+      }
+      
+      return 0;
+}

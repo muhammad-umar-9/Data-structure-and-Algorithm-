26fac405d68565b03c03949b6987eab36a5ee589
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-06-07 20:22:36 +0500

    delete element at any index

--- Pointers/delete_element_any_index.cpp
+++ Pointers/delete_element_any_index.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 using namespace std;
-
-//to delete the last element of array
-
 int main()
 {
       int a[] = {2 , 3 , 4 ,5 , 6 , 7};
       int n = sizeof(a) / sizeof(a[0]);
+     // int index_to_delete = 2;
 
       cout<<"orignal array"<<endl;
        for(int i = 0 ; i<n ; i++)
@@ -14,14 +12,15 @@ int main()
             cout<<a[i]<<"  ";
       }
       cout<<endl;
-      // for(int i = 0 ; i<=n-2; i++)
-      // {
-      //       a[i-1] = a[i];
-      // }
+      for(int i = 2 ; i<n-1;  i++) //start from index at which you delete the element 
+                                   //move backshift element
+      {
+            a[i] = a[i+1];
+      }
       n--;
       cout<<"Modified array"<<endl;
       
-      for(int i = 0 ; i<n-2 ; i++)
+      for(int i = 0 ; i<n ; i++)
       {
             
             cout<<a[i]<<"  ";

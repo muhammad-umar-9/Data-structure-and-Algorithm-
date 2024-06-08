38d09e7066ba06dae293eee9b14de4397fa1f573
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-06-08 09:13:26 +0500

    done

--- Arrays/delete_element_any_index.cpp
+++ Arrays/delete_element_any_index.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
       int a[] = {2 , 3 , 4 ,5 , 6 , 7};
@@ -13,7 +14,7 @@ int main()
       }
       cout<<endl;
       for(int i = 2 ; i<n-1;  i++) //start from index at which you delete the element 
-                                   //move backshift element
+                                  //move backshift element
       {
             a[i] = a[i+1];
       }
